0832c2a85d9c9d0d61c2f22e47471b2fcdabb845
Vinicius Campos Silva <vini.javac@gmail.com>
2015-08-10 21:51:52 -0300

    second chapter

---
+++ chapter_02.py
@@ -0,0 +1,16 @@
+lista = ['Vinicius Campos', 'Pedro Castro', 'Rodrigo Yokoyama']
+print lista[0]
+print lista[1]
+print lista[1:]
+
+lista.append('Felipe Martins')
+
+print lista[3]
+
+lista.append(8)
+
+print lista
+
+lista.remove(8)
+
+print lista
