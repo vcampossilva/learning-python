8d581fd70ab967084a75c204492945d0f1723315
Vinicius Campos Silva <vini.javac@gmail.com>
2015-08-10 23:21:02 -0300

    third chapter

---
+++ chapter_03.py
@@ -0,0 +1,29 @@
+#tuple
+
+tipos_convite = ['vip', 'normal', 'meia', 'cortesia']
+
+print tipos_convite
+
+tipos_convite.append('penetra')
+
+print tipos_convite
+
+tipos_convite = ('vip', 'normal', 'meia', 'cortesia')
+
+#dictionary
+
+convites = ('vip', 60, 'normal', 40, 'meia', 30, 'cortesia', 0)
+
+print convites
+
+print convites[0:2]
+
+convites_com_valor = {'vip': 60, 'normal' : 40, 'meia' : 30, 'cortesia' : 0}
+
+print convites_com_valor
+
+print convites_com_valor['vip']
+
+print convites_com_valor.keys()
+
+print convites_com_valor.values()
