06cb92c23cd96de83666d61604c31b8ea2751fcc
Vinicius Campos Silva <vini.javac@gmail.com>
2015-08-11 23:06:20 -0300

    tuplas and dictionary

--- chapter_03.py
+++ chapter_03.py
@@ -27,3 +27,41 @@ print convites_com_valor['vip']
 print convites_com_valor.keys()
 
 print convites_com_valor.values()
+
+status_civil = ('casado','solteiro') + ('divorciado',)
+
+print status_civil
+
+#verify type
+
+print type(convites_com_valor)
+print type(status_civil)
+
+#sum tuple, list
+
+nome = ['Vinicius', 'Rodrigo'] + ['Felipe', 'Pedro']
+
+print nome
+print type(nome)
+
+#sum tuple + list
+#estados = ('RJ','SP') + ['MG','ES'] -> generate a TypeError: can only concatenate list (not "tuple") to list
+
+#fixing
+estados = ('RJ', 'SP') + tuple(['MG', 'ES'])
+print estados
+print type(estados)
+
+#max/min number
+print max([10, 5, 7])
+print min([10, 5, 7])
+
+#sorted
+nomes = ['Vinicius', 'Rodrigo', 'Felipe', 'Pedro']
+print sorted(nomes)
+
+print 'exercicio'
+materias_com_peso = {'Equacoes Diofantinas' : 3, 'Algebra Relacional' : 2, 'Portugues instrumental' : 4}
+pesos = tuple(materias_com_peso.values())
+pesos.append(1)
+print pesos
